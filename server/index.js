36fb6eb17569fa89a4982a0b7d60af7d4d603d9e
Amit Giri <thisisag840@gmail.com>
2023-02-21 19:11:34 +0530

    initial setup

---
+++ server/index.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import bodyParser from 'bodyParser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+
+const app = express();
+
+app.use(bodyParser.json({limit:'30mb', extended:true}));
+app.use(bodyParser.urlencoded({limit:'30mb', extended:true}));
