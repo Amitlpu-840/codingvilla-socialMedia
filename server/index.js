03a29fdc50e7a5424a94677c3e16184529a3a3a9
Amit Giri <thisisag840@gmail.com>
2023-02-21 19:40:25 +0530

    added mongodb

--- server/index.js
+++ server/index.js
@@ -7,3 +7,5 @@ const app = express();
 
 app.use(bodyParser.json({limit:'30mb', extended:true}));
 app.use(bodyParser.urlencoded({limit:'30mb', extended:true}));
+app.use(cors());
+const CONNECTION_URL = 'mongodb+srv://Amit_Giri:<password>@cluster0.jqexwyl.mongodb.net/?retryWrites=true&w=majority'
