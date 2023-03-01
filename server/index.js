6a2c80065b2d683fd4de01376e8043f7598ad2f1
Amit Giri <thisisag840@gmail.com>
2023-03-01 19:26:05 +0530

    added postRouter

--- server/index.js
+++ server/index.js
@@ -2,16 +2,21 @@ import express from 'express'
 import bodyParser from 'body-Parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import postRouter from './routes/posts.js'
 
 const app = express();
+
+ 
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
+app.use('/posts', postRouter);
+
 
 const CONNECTION_URL = 'mongodb+srv://Amit_Giri:j0SVcxpJZh0oSetR@cluster0.jqexwyl.mongodb.net/?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true}).then(() => app.listen(PORT, () => console.log(`server is running on port number ${PORT}`))).catch((error) => console.log(error.message));
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => app.listen(PORT, () => console.log(`server is running on port number ${PORT}`))).catch((error) => console.log(error.message));
+//mongoose.set('useFindAnyModify', false);
 
-// mongoose.set('useFindAnyModify', false);
