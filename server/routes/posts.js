e77c98bc787e81e8eff193aba953519761b5d1ab
Amit Giri <thisisag840@gmail.com>
2023-03-01 19:25:35 +0530

    created

---
+++ server/routes/posts.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+import { createPost, getPosts } from "../controllers/posts.js";
+
+const router = express.Router();
+
+
+router.get('/', getPosts );
+router.get('/create', createPost );
+// router.get('/home',(req, res)=>{
+//     res.send('this is home!');
+// });
+
+export default router;
