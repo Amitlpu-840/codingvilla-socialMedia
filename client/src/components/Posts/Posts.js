49e18ce34405ab0f58a14fa31afdd737e5427508
Amit Giri <thisisag840@gmail.com>
2023-03-03 23:07:48 +0530

    inital redux and dispatch setup

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Post from './Post/Post'
-
+import { useSelector } from 'react-redux';
 import useStyles from './styles'
 
 function Posts() {
+    const posts = useSelector((state)=> state.posts);
     const classes = useStyles();
+    console.log(posts);
     return (
         <>
             <h1>
