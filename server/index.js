4e85c73e6f16b0d5c716ca08f8ed935ae677ee36
Amit Giri <thisisag840@gmail.com>
2023-03-04 18:58:14 +0530

    env variable setup

--- server/index.js
+++ server/index.js
@@ -3,18 +3,21 @@ import bodyParser from 'body-Parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import postRouter from './routes/posts.js'
+import dotenv from 'dotenv'
+
 
 const app = express();
 
- 
 
+
+dotenv.config();
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 app.use('/posts', postRouter);
 
 
-const CONNECTION_URL = 'mongodb+srv://Amit_Giri:j0SVcxpJZh0oSetR@cluster0.jqexwyl.mongodb.net/?retryWrites=true&w=majority'
+const CONNECTION_URL = process.env.REACT_APP_DATABASE_KEY
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => app.listen(PORT, () => console.log(`server is running on port number ${PORT}`))).catch((error) => console.log(error.message));
