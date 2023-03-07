94b219bb173c1b11e9968a2f1bd5c5f1ce746e2d
Amit Giri <thisisag840@gmail.com>
2023-03-07 20:05:38 +0530

    background, post and form updated

--- client/src/App.js
+++ client/src/App.js
@@ -11,33 +11,36 @@ import Form from './components/Form/Form';
 import useStyles from './styles'
 const App = () => {
 
-    const classes = useStyles();
-    const dispatch = useDispatch();
+  const classes = useStyles();
+  const dispatch = useDispatch();
 
-    useEffect(()=>{
-      dispatch(getPosts());
-    }, [dispatch] )
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch])
 
-    return (
-        
-        <Container maxWidth="lg">
-        <AppBar className={classes.appBar} position="static" color="inherit">
-          <Typography className={classes.heading} variant="h2" align="center">CodingVilla</Typography>
-          <img className={classes.image} src={memories} alt="icon" height="60" />
-        </AppBar>
-        <Grow in>
-          <Container>
-            <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
-              <Grid item xs={12} sm={7}>
-                <Posts />
-              </Grid>
-              <Grid item xs={12} sm={4}>
-                <Form/>
-              </Grid>
+  return (
+
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">CodingVilla</Typography>
+        <img className={classes.image} src={memories} alt="icon" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
             </Grid>
-          </Container>
-        </Grow>        
-      </Container>
-    );
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.footerHeading} variant="h4" align="center">Thanks for visiting. Development of other functionality is in porgress. Thanks!</Typography>
+      </AppBar>
+    </Container>
+  );
 }
 export default App;
--- client/src/styles.js
+++ client/src/styles.js
@@ -12,6 +12,9 @@ export default makeStyles(() => ({
   heading: {
     color: 'rgba(0,183,255, 1)',
   },
+  footerHeading: {
+    color: 'rgba(102,0,0, 1)',
+  },
   image: {
     marginLeft: '15px',
   },
