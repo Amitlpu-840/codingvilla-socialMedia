8b49d8a734315d7a97cc59d01ef381b67f9ce9f9
Amit Giri <thisisag840@gmail.com>
2023-03-10 23:25:50 +0530

    updatePost added

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,12 +1,14 @@
 import express from "express";
 
-import { createPost, getPosts } from "../controllers/posts.js";
+import { createPost, getPosts, updatePost } from "../controllers/posts.js";
 
 const router = express.Router();
 
 
 router.get('/', getPosts );
 router.post('/', createPost);
+router.patch('/:id', updatePost  );
+
 // router.get('/create', createPost );
 // router.get('/home',(req, res)=>{
 //     res.send('this is home!');
