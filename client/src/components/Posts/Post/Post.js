c18ef27edf405b89c836077f14e84b12ec9e391d
Amit Giri <thisisag840@gmail.com>
2023-03-12 21:51:18 +0530

    material ui error and post title

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -17,15 +17,15 @@ function Post({post, setCurrentId}) {
         </div>
         <div className={classes.overlay2} >
           <Button style={{color:'white'}} size='small' onClick={()=>  setCurrentId(post._id) }  >
-            <MoreHorizIcon fontSize='default' />
+            <MoreHorizIcon fontSize='medium' />
           </Button>
         </div>
         <div className={classes.details}>
           <Typography variant='body2' color='textSecondary'  >{post.tags.map((tag)=>`#${tag} `)} </Typography>
         </div>
+        <Typography className={classes.title} variant='h4' gutterBottom  >{post.title}</Typography>
         <CardContent>
-        <Typography className={classes.title} variant='h5' gutterBottom  >{post.message}</Typography>
-
+          <Typography variant='h6' gutterBottom  >{post.message}</Typography>
         </CardContent>
         <CardActions className={classes.cardActions} >
           <Button size='small' color='primary' onClick={()=>{}} >
