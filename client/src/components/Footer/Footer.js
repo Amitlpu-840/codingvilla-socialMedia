039bc03007e6b0b4114ada2e5429a379d1a37e93
Amit Giri <thisisag840@gmail.com>
2023-03-22 23:33:01 +0530

    footer component added

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { AppBar, Typography } from "@material-ui/core";
+import useStyles from './styles'
+
+function Footer() {
+const classes = useStyles();
+  return (
+    <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.footerHeading} variant="h4" align="center">Development of other functionality is in porgress. Thanks for visiting! </Typography>
+      </AppBar>
+  )
+}
+
+export default Footer
