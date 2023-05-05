ecab387f40a95ea2f69c0ea82fd471a65cedee3d
Amit Giri <thisisag840@gmail.com>
2023-05-05 22:18:25 +0530

    middleware for authorization added

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,19 +1,14 @@
 import express from "express";
-
+import auth from '../middleware/auth.js'
 import { createPost, getPosts, updatePost,deletePost, likePost } from "../controllers/posts.js";
 
 const router = express.Router();
 
 
 router.get('/', getPosts );
-router.post('/', createPost);
-router.patch('/:id', updatePost  );
-router.delete('/:id', deletePost);
-router.patch('/:id/likePost', likePost);
-
-// router.get('/create', createPost );
-// router.get('/home',(req, res)=>{
-//     res.send('this is home!');
-// });
+router.post('/',auth ,createPost);
+router.patch('/:id',auth ,updatePost  );
+router.delete('/:id',auth ,deletePost);
+router.patch('/:id/likePost',auth, likePost);
 
 export default router;
